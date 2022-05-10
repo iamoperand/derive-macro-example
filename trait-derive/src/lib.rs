f45fe64db39396ce43bccdda13a8b14af8108d9a
nikhil arora <narora200@gmail.com>
2022-05-10 18:34:22 +0530

    feat: full implementation with helper attributes

--- trait-derive/src/lib.rs
+++ trait-derive/src/lib.rs
@@ -1,3 +1,4 @@
+use darling::FromDeriveInput;
 use proc_macro::{self, TokenStream};
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
@@ -6,11 +7,31 @@ use syn::{parse_macro_input, DeriveInput};
 // #[my_trait(answer = 0)]
 // struct Bar;
 
-#[proc_macro_derive(MyTrait)]
+#[derive(FromDeriveInput, Default)]
+#[darling(default, attributes(my_trait))]
+struct Opts {
+    answer: Option<i32>,
+}
+
+#[proc_macro_derive(MyTrait, attributes(my_trait))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, .. } = parse_macro_input!(input);
+    let input = parse_macro_input!(input);
+    let opts = Opts::from_derive_input(&input).expect("wrong options");
+
+    let DeriveInput { ident, .. } = input;
+    let answer = match opts.answer {
+        Some(x) => quote! {
+            fn answer() -> i32 {
+                #x
+            }
+        },
+        None => quote! {},
+    };
+
     let output = quote! {
-        impl MyTrait for #ident {}
+        impl MyTrait for #ident {
+            #answer
+        }
     };
     output.into()
 }
